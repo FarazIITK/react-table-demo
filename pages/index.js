00f4fcf67849dd7a21732111f42fd7bed3349ca0
FarazIITK <fhusain@wr.iitr.ac.in>
2023-08-24 15:50:10 +0530

    Added table component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
+import { useMemo, useState } from "react";
 import styles from "../styles/Home.module.css";
+import { useTable, useGlobalFilter } from "react-table";
 
 export default function Home(props) {
-  const { products } = props;
+  const [products, setProducts] = useState(props.products);
+
+  const columns = [
+    {
+      Header: "Product Id",
+      accessor: "id",
+    },
+    {
+      Header: "Name",
+      accessor: "title",
+    },
+  ];
+
+  // Memoize
+  const tableColumns = useMemo(() => columns, []);
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns: tableColumns, data: products });
 
   return (
     <div className={styles.main}>
@@ -10,6 +29,36 @@ export default function Home(props) {
       </div>
       <div className={styles.pageBody}>
         <h3>Details of users...</h3>
+      </div>
+
+      <div className={styles.tableStyles}>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
